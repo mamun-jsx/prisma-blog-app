6c07910da443c63efadb402a750de773d8077ae4
almamun528 <ayapths@gmail.com>
2026-01-06 00:21:48 +0600

    seedAdmin fnc

---
+++ src/script/seedAdmin.ts
@@ -0,0 +1,37 @@
+import { prisma } from "../lib/prisma";
+import { UserRole } from "../modules/post/post.router";
+
+//
+async function seedAdmin() {
+  try {
+    const adminData = {
+      name: "admin Saheb",
+      email: "admin@admin.com",
+      role: UserRole.ADMIN,
+      password: "admin1234",
+
+    };
+    // check dataBase user is exist or not....
+    const existingUser = await prisma.user.findUnique({
+      where: { email: adminData.email },
+    });
+    if (existingUser) {
+      throw new Error("User already exists");
+    }
+
+    const signUpAdmin = await fetch(
+      "http://localhost:3001/api/auth/sign-up/email",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(adminData),
+      }
+    );
+    console.log(signUpAdmin);
+  } catch (error: any) {
+    console.log(error);
+  }
+}
+seedAdmin();
