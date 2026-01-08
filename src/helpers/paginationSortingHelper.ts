51e75098d33a261d8b89d7e6a10395caffcac8f8
almamun528 <ayapths@gmail.com>
2026-01-08 23:59:44 +0600

    helper function

---
+++ src/helpers/paginationSortingHelper.ts
@@ -0,0 +1,31 @@
+//  pagination and sorting request type/schema
+
+type IOptions = {
+  page?: number | string;
+  limit?: number | string;
+  sortBy?: string;
+  sortOrder?: string;
+};
+
+type IOptionResult = {
+  page: number;
+  limit: number;
+  skip: number;
+  sortBy: string;
+  sortOrder: string;
+};
+const paginationSortingHelper = (options: IOptions): IOptionResult => {
+  //   pagination
+  const page: number = Number(options.page) || 1;
+  const limit: number = Number(options.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  //   sorting
+
+  const sortBy: string = options.sortBy || "createdAt";
+  const sortOrder: string = options.sortOrder || "desc";
+
+  return { page, limit, skip, sortBy, sortOrder };
+};
+
+export default paginationSortingHelper;
--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { postService } from "./post.service";
 import { PostStatus } from "../../../generated/prisma/enums";
+import paginationSortingHelper from "../../helpers/paginationSortingHelper";
 
 const createPost = async (req: Request, res: Response) => {
   try {
@@ -39,13 +40,11 @@ const getAllPost = async (req: Request, res: Response) => {
     // authorId
     const authorId = req.query.authorId as string | undefined;
 
-    //  pagination
-    const page = Number(req.query.page ?? 1);
-    const limit = Number(req.query.limit ?? 10);
-    const skip = (page - 1) * limit;
-    // sorting by
-    const sortBy = req.query.sortBy as string | undefined;
-    const sortOrder = req.query.sortOrder as string | undefined;
+    //* pagination and sorting
+    const { page, limit, skip, sortBy, sortOrder } = paginationSortingHelper(
+      req.query
+    );
+
     //------------------------- result------------------
     const result = await postService.getAllPost({
       search: searchString,
